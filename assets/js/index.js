5e2219935d7be04f909baeef9d7a56a5d7ecfb98
beto <alberto.luna87@hotmail.com>
2024-06-16 15:54:41 +1200

    desafio3

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+
+
+function cambiaBackground(elementId, color = 'black') {
+    elemento = document.querySelector('#' + elementId);
+    elemento.style.backgroundColor= color
+    }
+    
+
+        
+
+
+
